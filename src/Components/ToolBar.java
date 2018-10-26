51ef417a52543c8d75ccc9ae507f90e2ce0d3d51
FlikHu <hah045@ucsd.edu>
2018-10-26 11:14:35 -0700

    WIP

--- src/Components/ToolBar.java
+++ src/Components/ToolBar.java
@@ -1,57 +1,140 @@
 package Components;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.concurrent.Worker;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebHistory;
+import javafx.scene.web.WebView;
 
-public class ToolBar extends VBox {
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
-    public ToolBar() {
-        HBox urlField = new HBox();
+public class ToolBar extends HBox {
+
+    // Top tool bar
+    public ToolBar(WebView view, String hPage) {
+        WebEngine engine = view.getEngine();
+        WebHistory history = engine.getHistory();
+
+        // Buttons
         TextField url = new TextField();
+        url.setText(engine.getLocation());
 
         Button back = new Button();
+        back.setDisable(true);
         Button forward = new Button();
+        forward.setDisable(true);
         Button refresh = new Button();
+        Button homepage = new Button();
         Button go = new Button();
 
         back.setText("<-");
-        back.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-
-            }
-        });
+        back.setOnAction((event-> {
+                history.go(-1);
+        }));
 
         forward.setText("->");
-        forward.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
+        forward.setOnAction((event->{
+                history.go(1);
+        }));
 
-            }
-        });
+        refresh.setText("R");
+        refresh.setOnAction((event -> {
+            engine.reload();
+        }));
 
-        refresh.setText("*");
-        refresh.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-
-            }
-        });
+        homepage.setText("H");
+        homepage.setOnAction((event -> {
+            engine.load(hPage);
+        }));
 
         go.setText("Go");
-        go.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
+        go.setOnAction((event -> {
+            String temp = url.getText();
+            if(temp.length() == 0) {
+                url.setText("Invalid URL");
+            } else {
+                if(!temp.startsWith("http://") || !temp.startsWith("https://")) {
+                    temp = "https://"+temp;
+                }
+                try {
+                    URL address = new URL(temp);
+                    URLConnection connection = address.openConnection();
+                    connection.connect();
+                    engine.load(temp);
+                } catch (MalformedURLException e1 ){
+                    url.setText("Invalid URL");
+                } catch (IOException e2) {
+                    url.setText("Please try again later :(");
+                }
+            }
+        }));
 
+        history.currentIndexProperty().addListener((observable, oldValue, newValue) -> {
+            int historyIndex = newValue.intValue();
+            if(historyIndex <= 0) {
+                back.setDisable(true);
+            } else {
+                back.setDisable(false);
+            }
+
+            if(historyIndex >= history.getEntries().size()-1) {
+                forward.setDisable(true);
+            } else {
+                forward.setDisable(false);
             }
         });
 
-        urlField.getChildren().addAll(back, forward, refresh, url, go);
+        engine.locationProperty().addListener((observable, oldURL, newURL) -> {
+            url.setText(newURL);
+        });
+
+        this.setFillHeight(true);
+        this.setAlignment(Pos.CENTER_LEFT);
+        this.getStyleClass().add("bar");
         this.getStylesheets().add("Style/Style.css");
-        this.getChildren().addAll(new BookMark(), urlField);
+        this.getChildren().addAll(back, forward, refresh, homepage, url, go);
+    }
+
+    // Bottom tool bar
+    public ToolBar(WebView view) {
+        WebEngine engine = view.getEngine();
+        Worker worker = engine.getLoadWorker();
+
+        ProgressBar loadingProgress = new ProgressBar(0);
+
+        Label loadingLabel = new Label();
+        loadingLabel.setLabelFor(loadingProgress);
+        loadingLabel.setText("Loading:");
+
+        Button setting = new Button();
+        setting.setText("Setting");
+        setting.setId("setting");
+
+        Pane placeholder = new Pane();
+        placeholder.setPrefHeight(20);
+        this.setHgrow(placeholder, Priority.ALWAYS);
+
+        worker.progressProperty().addListener(((observable, oldProgress, newProgress) -> {
+            double progress = newProgress.doubleValue();
+            if(progress > 0) {
+                loadingProgress.setProgress(progress);
+            }
+        }));
+
+        this.setFillHeight(true);
+        this.setAlignment(Pos.CENTER);
+        this.getStyleClass().add("bar");
+        this.getStylesheets().add("Style/Style.css");
+        this.getChildren().addAll(loadingLabel, loadingProgress, placeholder, setting);
     }
 }
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,6 +4,7 @@ import Components.ToolBar;
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Tab;
@@ -19,43 +20,51 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         String homepage = "https://stackoverflow.com/";
         TabPane webTabs = new TabPane();
-        primaryStage.setTitle("Welcome to JFX Browser");
+        primaryStage.setTitle("JFXBrowser");
 
         welcomeTab(webTabs);
         Tab plus = new Tab(" + ");
         plus.closableProperty().setValue(false);
         webTabs.getTabs().add(plus);
-        webTabs.getStyleClass().add("web_tabs");
         webTabs.getStylesheets().add("Style/Style.css");
+        webTabs.prefWidthProperty().bind(primaryStage.widthProperty().multiply(1));
+        webTabs.prefHeightProperty().bind(primaryStage.heightProperty().multiply(0.97));
+
         webTabs.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Tab>() {
             @Override
             public void changed(ObservableValue<? extends Tab> observable, Tab oldValue, Tab newValue) {
                 if (newValue == plus) {
-                    Tab newTab = new Tab("newnewnwe");
+                    Tab newTab = new Tab();
                     webTabs.getTabs().remove(plus);
                     webTabs.getTabs().addAll(newTab, plus);
+
+                    WebView view = new WebView();
+                    WebEngine engine = view.getEngine();
+                    engine.load(homepage);
+
+                    engine.titleProperty().addListener(((observableValue, oldName, newName) -> {
+                        newTab.setText(newName);
+                    }));
+
+                    VBox box = new VBox();
+                    box.getChildren().addAll(new ToolBar(view, homepage), view, new ToolBar(view));
+                    view.prefHeightProperty().bind(webTabs.heightProperty());
+                    view.getStyleClass().add("view");
+
+
+                    newTab.setContent(box);
                     webTabs.getSelectionModel().select(newTab);
                 } else {
-
+                    webTabs.getSelectionModel().select(newValue);
                 }
             }
         });
 
         Group root = new Group();
-
-        WebView view = new WebView();
-        WebEngine engine = view.getEngine();
-        engine.load(homepage);
-        view.setStyle("-fx-pref-width: 1000px");
-
-        VBox box = new VBox();
-        box.getChildren().addAll(new ToolBar(), view);
-
-
         root.getChildren().addAll(webTabs);
         Scene main = new Scene(root,1000, 800);
         primaryStage.setScene(main);
-        primaryStage.setResizable(false);
+        primaryStage.setResizable(true);
         primaryStage.show();
     }
 
